a92bef33f40d7b8cd78d20356470a4cf3ab94c09
Scott Clingan <sclingan1@gmail.com>
2021-05-19 15:44:17 -0500

    styling contact page

--- src/Contact.js
+++ src/Contact.js
@@ -4,11 +4,17 @@ import React from 'react';
 function Contact(){
     return(
         <main className='Contact'>
-            <div id='heading'>Contact Me                                                    {/*fix this br tag */}
-                <div id='contact_p'>Please E-mail me with any Questions or inquiries. I would be <br />happy
+            <div id='heading'>Contact Me                                                   
+                <div id='contact_p'>Please E-mail me with any Questions or inquiries. I would be happy
                  to answer your questions or set up a meeting with you.
                 </div>
             </div>
+            <form id='contact_form'>
+                Name:<input></input>
+                E-mail:<input></input>
+                Subject:<input></input>
+                Message:<input></input>
+            </form>
         </main>
     )
 }
