d87f8e1e8bc391ea0fa87fa2e82af846efb86a55
Scott Clingan <sclingan1@gmail.com>
2021-05-22 23:25:34 -0500

    started about page

--- src/About.js
+++ src/About.js
@@ -6,6 +6,14 @@ function About(){
         <div id='about_title'>
             About Me
         </div>
+        <div id='about_p'>
+            This is just a test paragraph to see how well the 
+            text shows up against the background.
+        </div>
+        <div id='about_p2'>
+            This is just a test paragraph to see how well the 
+            text shows up against the background.
+        </div>
         </main>
     )
 }
