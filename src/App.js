619fdff21b6b339c30a90d9038594396ef04da57
Scott Clingan <sclingan1@gmail.com>
2021-05-26 10:04:20 -0500

    styling nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Projects from './Projects';
 import About from './About';
 import Contact from './Contact';
