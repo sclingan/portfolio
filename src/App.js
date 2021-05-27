477c6d8d707395c36e28097e9a4976a44d4ed616
Scott Clingan <sclingan1@gmail.com>
2021-05-27 11:50:32 -0500

    added some copy and styles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import About from './About';
 import Contact from './Contact';
 import Home from './Home';
 
+/* TODO
+   fix SASS, use variables, extends and mixins
+   set up e-mail on contact page, ensure form works as intended
+   fix home page and / issue in react router
+   set up the nav component so it shows on all pages
+   clean up code and add any comments that are needed˝
+*/
+
 function App() {  
   return (
     <main className="Main">
