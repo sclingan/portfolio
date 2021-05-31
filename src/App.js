2b8fccbcf5835c519e3a648116b31ddb288fdd2a
Scott Clingan <sclingan1@gmail.com>
2021-05-31 07:10:42 -0500

    added variables to SASS

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Home from './Home';
 /* TODO
    fix SASS, use variables, extends and mixins
    set up e-mail on contact page, ensure form works as intended
-   fix home page and / issue in react router
    set up the nav component so it shows on all pages
    clean up code and add any comments that are needed
 */
@@ -22,7 +21,7 @@ function App() {
            <Route path='/About' component={About}/>
            <Route path='/Contact' component={Contact}/>
            <Route path='/Projects' component={Projects} exact/>
-           <Route path='/' component={Home} exact/>   {/* fix this so that / and /Home land on the same page */}
+           <Route path='/' component={Home} exact/>  
          </Switch>
     </Router>
     </main>
