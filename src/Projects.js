c87016c78ae6becdbc95cf5740c8d8984d303015
Scott Clingan <sclingan1@gmail.com>
2021-06-14 20:08:44 -0500

    mobile first responsive

--- src/Projects.js
+++ src/Projects.js
@@ -13,31 +13,31 @@ function Projects(){ /* add more projects!! */
         <main className='Projects'>
          <div id='projects-title'>Projects</div>
          <div id='project_1'>Random Quote Machine<br />
-         <img id='project_randomQuote' src={randomquote} alt='Random Quote Generator'></img>
+             <img id='project_randomQuote' src={randomquote} alt='Random Quote Generator'></img>
          </div>
          <div id='project_2'>United States GDP<br />
-         <img id='project_usGdp' src={gdp} alt='United States GDP'></img>
+             <img id='project_usGdp' src={gdp} alt='United States GDP'></img>
          </div>
          <div id='project_3'>Coffee Shop<br />
-         <img id='project_Coffee' src={coffee} alt='Coffee Shop'></img>
+             <img id='project_Coffee' src={coffee} alt='Coffee Shop'></img>
          </div>
          <div id='project_4'>Drum Machine<br />
-         <img id='project_drum' src={drum} alt='Drum Machine'></img>
+             <img id='project_drum' src={drum} alt='Drum Machine'></img>
          </div>
          <div id='project_5'><br />
-         {/*<img id='project_' src='' alt=''></img>*/}
+              {/*<img id='project_' src='' alt=''></img>*/}
          </div>
          <div id='project_6'>Pomodoro<br />
-         <img id='project_pomodoro' src={pomodoro} alt='Pomodoro'></img>
+             <img id='project_pomodoro' src={pomodoro} alt='Pomodoro'></img>
          </div>
          <div id='project_7'>U.S Bar Chart<br />
-         <img id='project_usBar' src={barChart} alt='U.S Bar Chart'></img>
+             <img id='project_usBar' src={barChart} alt='U.S Bar Chart'></img>
          </div>
          <div id='project_8'>More to come<br />
-         <img id='project_' src='' alt=''></img>
+             <img id='project_' src='' alt=''></img>
          </div>
          <div id='project_9'>More to come<br />
-         <img id='project_' src='' alt=''></img>
+            <img id='project_' src='' alt=''></img>
          </div>
          <Nav />
         </main>
